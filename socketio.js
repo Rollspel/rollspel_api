8b4e4c54b555c8722af0dc93f18843c4d77b39c7
Herael <b.rousval@hotmail.com>
2020-05-17 19:02:55 +0200

    test socket.io :rocket:

--- socketio.js
+++ socketio.js
@@ -5,6 +5,19 @@ const socketIO = {
         const ioServer = io(server);
         ioServer.on('connection', socket => {
             console.log('connection : ', socket.id);
+            socket.emit('message', "vous êtes bien connecté !");
+            // socket.broadcast.emit('message', 'Un autre client vient de se connecter !');
+
+            // Quand le serveur reçoit un signal de type "message" du client
+            socket.on('message', function (message) {
+                console.log(socket.pseudo + " me parle ! Il me dit : " + message);
+            });
+
+            //Stocker des informations dans un socket. Genre un pseudo
+            socket.on('petit_nouveau', function(pseudo) {
+                socket.pseudo = pseudo;
+            });
+
             socket.on('disconnect', () => {
             console.log('disconnection : ', socket.id);
             });
