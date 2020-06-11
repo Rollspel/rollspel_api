f1a6b118cfaf2fa12bf903a02f449b0d5df983f1
Tristan Luong <tristan.luong@gmail.com>
2020-06-12 00:21:36 +0200

    Link app to gameboard, update board on tap

--- socketio.js
+++ socketio.js
@@ -1,27 +1,40 @@
 const io = require('socket.io');
 
+let users = [];
+let gameboards = [{ socketID: "12345", gameboardID: "12345" }];
+
+setInterval(() => {
+    console.log('users : ', users);
+}, 10000);
+
 const socketIO = {
     start: server => {
         const ioServer = io(server);
         ioServer.on('connection', socket => {
 
-            if (socket.isGameboard === true) {
-                console.log('Gameboard connection : ', socket.id);
-                socket.emit('connectGameboard', 1);
+            // Add a new gameboard 
+            socket.on('add_gameboard', (gameboardID) => {
+                gameboards.push({ socketID: socket.id, gameboardID: gameboardID });
+            });
 
-                socket.on('createGameboard', function (gameboardID) {
-                    socket.gameboardID =  gameboardID;
-                    socket.isConnectedToPlayer = false;
-                });
-            }
+            // Link user to a gameboard ID;
+            socket.on('player_link_gameboard', (gameboardID) => {
+                console.log('A player link his gameboard');
+                if(gameboards.find(gameboard => gameboard.gameboardID === gameboardID)){
+                    users.push({ socketID: socket.id, gameboardID: gameboardID });
+                    socket.emit('player_link_gameboard_success');
+                } else {
+                    socket.emit('player_link_gameboard_fail');
+                }
+            });
 
-            console.log('connection : ', socket.id);
-            socket.emit('message', "vous êtes bien connecté !");
-            // socket.broadcast.emit('message', 'Un autre client vient de se connecter !');
-
-            // Quand le serveur reçoit un signal de type "message" du client
-            socket.on('message', function (message) {
-                console.log(socket.username + " me parle ! Il me dit : " + message);
+            socket.on('player_tap', (data) => {
+                console.log(data);
+                const user = users.find(user => user.gameboardID === data.gameboardID);
+                if(user){
+                    console.log('Sending new board');
+                    ioServer.to(user.socketID).emit('player_receive_new_board', data.board);
+                }
             });
 
             //Stocker des informations dans un socket. Genre un pseudo
